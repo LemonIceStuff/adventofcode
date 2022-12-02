e2b73123ac1bc04f02e64986057717ff6371af20
LemonIceStuff <43790892+LemonIceStuff@users.noreply.github.com>
2022-12-02 14:05:33 +0100

    Day 1 puzzle

--- Day1.py
+++ Day1.py
@@ -1,3 +1,38 @@
-#transform text to list
+#Part 1
 
-input = ""
+input = open("puzzle_input.txt", "r")
+
+line = input.readline()
+max_calories = 0
+calories = 0
+while line != "":
+    if len(line)>1:
+        calories += int(line)
+    else:
+        if calories > max_calories:
+            max_calories = calories
+        calories = 0
+    line = input.readline()
+
+print(max_calories)
+input.close()
+
+#Part 2
+
+input = open("puzzle_input.txt", "r")
+
+line = input.readline()
+max_calories = [0,0,0]
+calories = 0
+while line != "":
+    if len(line)>1:
+        calories += int(line)
+    else:
+        max_calories.sort()
+        if calories > max_calories[0]:
+            max_calories[0] = calories
+        calories = 0
+    line = input.readline()
+
+print(sum(max_calories))
+input.close()
