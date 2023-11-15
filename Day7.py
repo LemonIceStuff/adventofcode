cbefafffedcf2e9d63bcd570890731125c587189
LemonIceStuff <43790892+LemonIceStuff@users.noreply.github.com>
2023-11-15 16:43:51 +0100

    tried sum stuff

--- Day7.py
+++ Day7.py
@@ -1,12 +1,33 @@
+import re
+
+input = open("puzzle_input.txt", "r")
+
 class Directory:
-    def __init__(self, parent, child, files):
+    def __init__(self, parent, name, files):
         self.parent = parent
-        self.child = child
+        self.name = name
         self.files = files
 
 
 class File:
-    def __init__(self, size, parent):
+    def __init__(self, size, parent, name):
         self.parent = parent
+        self.name = name
         self.size = size
 
+
+line = input.readline()
+directory = []
+files = []
+
+directory.append(Directory("", "/", ""))
+cur_dir = directory[0] #We start at root
+
+while line != "":
+    if line[0] == "$":
+        if "cd" in line :
+            cur_dir = re.findall("cd (.+)", line)
+
+
+
+    line = input.readline()
